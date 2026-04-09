dd2587f5c188b231226ff5cf2e1f7770d99037e2
Cristian Gracia <est.cristian.gracia@unimilitar.edu.co>
2026-04-09 10:55:28 -0500

    Se optimizó el main con el uso de metodos

--- src/main/java/com/mycompany/ordenamientoburbuja/OrdenamientoBurbuja.java
+++ src/main/java/com/mycompany/ordenamientoburbuja/OrdenamientoBurbuja.java
@@ -9,7 +9,14 @@ public class OrdenamientoBurbuja {
         
         System.out.println(Arrays.toString(arr));
         
-        int n = arr.length;
+        arr = ordenBurbuja(arr);
+        
+        System.out.println(Arrays.toString(arr));
+    }
+    public static int[] ordenBurbuja(int arr[]) {
+        int auxArr[] = new int[arr.length];
+        System.arraycopy(arr, 0, auxArr, 0, 0);
+         int n = arr.length;
         for (int i = 0; i < n-1; i++){
             for (int j = i+1; j < n; j++) {
                 if(arr[i] > arr[j]) {
@@ -19,6 +26,6 @@ public class OrdenamientoBurbuja {
                 }
             }
         }
-        System.out.println(Arrays.toString(arr));
+    return arr;
     }
 }
