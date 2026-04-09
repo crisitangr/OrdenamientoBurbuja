d49e32943be062f699b62d7e47ac570fc6662c50
Cristian Gracia <est.cristian.gracia@unimilitar.edu.co>
2026-04-09 10:13:40 -0500

    se implemento el arreglo con burbuja comparando los elementos y ordenandolos

---
+++ src/main/java/com/mycompany/ordenamientoburbuja/OrdenamientoBurbuja.java
@@ -0,0 +1,24 @@
+
+package com.mycompany.ordenamientoburbuja;
+import java.util.Arrays;
+
+public class OrdenamientoBurbuja {
+
+    public static void main(String[] args) {
+        int [] arr = {45, 22, 11, -4, 7, 3};
+        
+        System.out.println(Arrays.toString(arr));
+        
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++){
+            for (int j = i+1; j < n; j++) {
+                if(arr[i] > arr[j]) {
+                    int aux = arr [j];
+                    arr[j] = arr[i];
+                    arr[i] = aux;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
